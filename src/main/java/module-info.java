8647767bfa1d0bfceaad3ee6adc8e229c602832a
MelonIs45 <ryanpietras45@gmail.com>
2023-11-17 14:22:13 +0000

    main files

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example._cs250a2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens com.example._cs250a2 to javafx.fxml;
+    exports com.example._cs250a2;
+}
