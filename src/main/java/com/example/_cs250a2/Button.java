c00bd960cbc07795fb258b8f74496fff218b2329
jndmh <92041858+tinho-mp4@users.noreply.github.com>
2023-11-28 21:56:43 +0000

    Added Button.java + Trap.java Methods and Classes - Added Button Pressed - Linked Button pressed with Trap - When Actor goes on it this would activate
    + Some Unused Methods that needs to be used

--- src/main/java/com/example/_cs250a2/Button.java
+++ src/main/java/com/example/_cs250a2/Button.java
@@ -1,5 +1,30 @@
 package com.example._cs250a2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Button {
-    public static void event(){}
+    private boolean isPressed;
+    private final List<Trap> linkedTraps;
+
+    public Button() {
+        isPressed = false;
+        linkedTraps = new ArrayList<>();
+    }
+
+    public void linkTrap(Trap trap) {
+        linkedTraps.add(trap);
+    }
+
+    public void setPressed(boolean isPressed) {
+        this.isPressed = isPressed;
+        for (Trap trap : linkedTraps) {
+            trap.setActive(!isPressed);
+        }
+    }
+
+    public boolean isPressed() {
+        return isPressed;
+    }
 }
+
--- src/main/java/com/example/_cs250a2/Trap.java
+++ src/main/java/com/example/_cs250a2/Trap.java
@@ -1,5 +1,20 @@
 package com.example._cs250a2;
 
 public class Trap {
-    public static void event(){}
+    private boolean isActive;
+    public Trap() {
+        isActive = true;
+    }
+
+    public void setActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public boolean canMoveOff() {
+        return !isActive;
+    }
 }
