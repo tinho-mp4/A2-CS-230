fdda6bbd430356dddce0ed7b35e4011a2cf6eb7b
MelonIs45 <ryanpietras45@gmail.com>
2023-11-28 17:36:27 +0000

    Added Block and Water tile textures

--- src/main/java/com/example/_cs250a2/tile/WaterTile.java
+++ src/main/java/com/example/_cs250a2/tile/WaterTile.java
@@ -1,12 +1,13 @@
 package com.example._cs250a2.tile;
 
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 
 class WaterTile extends Tile {
+    private static final Image WATER_TILE = new Image(WaterTile.class.getResourceAsStream("/com/example/_cs250a2/Water.png"));
     @Override
     public void draw(GraphicsContext gc, double x, double y, double size) {
-        gc.setFill(javafx.scene.paint.Color.BLUE);
-        gc.fillRect(x, y, size, size);
+        gc.drawImage(WATER_TILE, x, y, size, size);
     }
 
     String getText() {
