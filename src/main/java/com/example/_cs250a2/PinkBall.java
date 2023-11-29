5b1e3f0735d88ef6f38c3ec96f726606fb9046e9
nocturnalelk07 <finnp@outlook.com>
2023-11-29 15:01:18 +0000

    most of monster added, working on pink ball
    constructor finished for pink ball and an initial design for the move method, still need to write checkTile and the rest of move (down, left, right)

--- src/main/java/com/example/_cs250a2/PinkBall.java
+++ src/main/java/com/example/_cs250a2/PinkBall.java
@@ -1,4 +1,36 @@
 package com.example._cs250a2;
 
+import java.util.ArrayList;
+
+/**
+ * javadoc to go here
+ */
 public class PinkBall extends Monster {
+
+    //pinkBall's own constructor
+    public PinkBall(ArrayList<Tile> tiles, int speed, char startingDirection, int[] startingLocation) {
+        blockingTiles = tiles;
+        this.speed = speed;
+        direction = startingDirection;
+        location = startingLocation;
+        monsterX = location[0];
+        monsterY = location[1];
+    }
+
+    //pink ball move method to be called by tick
+    @Override
+    public void move() {
+        //w is up
+        if (direction == 'w') {
+            //next location is 1 tile above current position
+            int[] locationNext = {monsterX, monsterY++};
+            //method to check the tile is legal
+            if (checkTile(locationNext)) {
+                monsterY = monsterY++;
+            } else {
+                direction = 's';
+                move();
+            }
+        }
+    }
 }
