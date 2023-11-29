675161e764eac079198f99c5ac050efaaebd25d0
nocturnalelk07 <finnp@outlook.com>
2023-11-29 15:48:04 +0000

    movement for pinkball should be working once i write checktile
    added if statements for the rest of the directions, not commented completely because they all work the same way and are explained by association

--- src/main/java/com/example/_cs250a2/PinkBall.java
+++ src/main/java/com/example/_cs250a2/PinkBall.java
@@ -18,19 +18,49 @@ public class PinkBall extends Monster {
     }
 
     //pink ball move method to be called by tick
-    
+
     public void move() {
-        //w is up
+
+        //w is up, s is down, a is left and d is right
         if (direction == 'w') {
             //next location is 1 tile above current position
             int[] locationNext = {monsterX, monsterY++};
             //method to check the tile is legal
             if (checkTile(locationNext)) {
-                monsterY = monsterY++;
+                monsterY++;
             } else {
                 direction = 's';
                 move();
             }
-        }
+
+        } else if (direction == 's') {
+            //next location is 1 tile below current position
+            int[] locationNext = {monsterX, monsterY--};
+            //check tile legality
+            if (checkTile(locationNext)) {
+                monsterY--;
+            } else {
+                direction = 'w';
+                move();
+            }
+
+        } else if (direction == 'a') {
+            int[] locationNext = {monsterX--, monsterY};
+            if (checkTile(locationNext)) {
+                monsterX--;
+            } else {
+                direction = 'd';
+                move();
+            }
+
+        } else if (direction == 'd') {
+            int[] locationNext = {monsterX++, monsterY};
+            if (checkTile(locationNext)) {
+                monsterX++;
+            } else {
+                direction = 'a';
+                move();
+            }
+    }
     }
 }
