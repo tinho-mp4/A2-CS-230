d78d94bf55c19710c6e08c01bec524daf3b592d4
Mwenya Sikazwe <>
2023-12-01 09:12:49 +0000

    Moved Chip Class (put it in the wrong place in my previous commit)

---
+++ src/main/java/com/example/_cs250a2/Chip.java
@@ -0,0 +1,15 @@
+package com.example._cs250a2;
+import java.util.ArrayList;
+
+public class Chip {
+
+    public Chip() {
+
+    }
+
+    public void collectChip(Player player) {
+
+        player.addToInventory("ComputerChip");
+
+    }
+}
--- src/main/java/com/example/_cs250a2/tile/Chip.java
+++
@@ -1,5 +0,0 @@
-/*package com.example._cs250a2.tile;
-
-public class Chip {
-}
-*/
