4103712630ded02b9b7b37db66d7b224dadd242a
MelonIs45 <ryanpietras45@gmail.com>
2023-12-03 10:05:23 +0000

    Added getter and setter to Tile x and y

--- src/main/java/com/example/_cs250a2/Tile.java
+++ src/main/java/com/example/_cs250a2/Tile.java
@@ -11,6 +11,22 @@ public class Tile {
         this.y = y;
     }
 
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
     public String getName() {
         return name;
     }
