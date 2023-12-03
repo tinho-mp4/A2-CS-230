e71c4de193b4d2fa2241db929b0b46c5ce6f1b97
nocturnalelk07 <finnp@outlook.com>
2023-12-03 16:51:23 +0000

    merged move and pathfind, wrote move for frog
    move method is hard coded for the frog with if statements, might require refactoring later and testing for corners etc

--- src/main/java/com/example/_cs250a2/Frog.java
+++ src/main/java/com/example/_cs250a2/Frog.java
@@ -3,7 +3,7 @@ package com.example._cs250a2;
 /**
  * javadoc to go here
  */
-//TODO write pathfind and move
+//TODO test when level available
 public class Frog extends Monster {
 
     public Frog (int speed, char startingDirection, int[] startingLocation) {
@@ -19,9 +19,60 @@ public class Frog extends Monster {
 
     //this move method will try and make the frog x equal to player x then do the same with y
     public void move() {
-        int[] checkLeft = {monsterX--,monsterY};
-        if (monsterX > Player.getX() && checkTile(checkLeft)) {
-
+        int[] checkLeft = {monsterX--, monsterY};
+        int[] checkRight = {monsterX++, monsterY};
+        int[] checkBelow = {monsterX, monsterY--};
+        int[] checkAbove = {monsterX, monsterY++};
+        int playerX = Player.getX;
+        int playerY = Player.getY;
+        if (monsterX > playerX && checkTile(checkLeft)) {
+            monsterX--;
+            direction = 'a';
+        } else if (monsterX < playerX && checkTile(checkRight)) {
+            monsterX++;
+            direction = 'd';
+        } else if (monsterY > playerY && checkTile(checkBelow)) {
+            monsterY--;
+            direction = 's';
+        } else if (monsterY < playerY && checkTile(checkAbove)) {
+            monsterY++;
+            direction = 'w';
+        } else if (direction == 'a' && checkTile(checkAbove)) {
+            monsterY++;
+            direction = 'w';
+        } else if (direction == 'a' && checkTile(checkBelow)) {
+            monsterY--;
+            direction = 's';
+        } else if (direction == 'a') {
+            monsterX++;
+            direction = 'd';
+        } else if (direction == 'w' && checkTile(checkLeft)) {
+            monsterX--;
+            direction = 'a';
+        } else if (direction == 'w' && checkTile(checkRight)) {
+            monsterX++;
+            direction = 'd';
+        } else if (direction == 'w') {
+            monsterY--;
+            direction = 's';
+        } else if (direction == 's' && checkTile(checkRight)) {
+            monsterX++;
+            direction = 'd';
+        } else if (direction == 's' && checkTile(checkLeft)) {
+            monsterX--;
+            direction = 'a';
+        } else if (direction == 's') {
+            monsterY++;
+            direction = 'w';
+        } else if (direction == 'd' && checkTile(checkBelow)) {
+            monsterY--;
+            direction = 's';
+        } else if (direction == 'd' && checkTile(checkAbove)) {
+            monsterY++;
+            direction = 'w';
+        } else if (direction == 'd') {
+            monsterY--;
+            direction = 'a';
         }
     }
 }
