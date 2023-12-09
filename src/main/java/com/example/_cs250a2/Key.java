b4022d160d34d920471346ce80000d20162c6cd4
peleWorks <2014459@swansea.ac.uk>
2023-12-09 12:20:07 +0000

    added Key functionality

--- src/main/java/com/example/_cs250a2/Key.java
+++ src/main/java/com/example/_cs250a2/Key.java
@@ -3,15 +3,42 @@ package com.example._cs250a2;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 
+enum KeyColour {RED, GREEN, BLUE, YELLOW};
+
+
 public class Key extends Item {
     private static final Image KEY_IMAGE = new Image(Key.class.getResourceAsStream("sprites/key.png"));
+    private KeyColour keyColour;
 
-    public Key(int x, int y) {
-        super("key", x, y, false);
+    public Key(int x, int y, char keyColourChar) {
+        super("key", x, y);
+        this.keyColour = getKeyColour(keyColourChar);
+    }
+
+
+
+    public KeyColour getKeyColour() {
+        return keyColour;
+    }
+
+    private KeyColour getKeyColour(char keyColourChar) {
+        return switch (keyColourChar) {
+            case 'R' -> KeyColour.RED;
+            case 'G' -> KeyColour.GREEN;
+            case 'B' -> KeyColour.BLUE;
+            case 'Y' -> KeyColour.YELLOW;
+            default -> null;
+        };
     }
 
     @Override
     public void draw(GraphicsContext gc, double x, double y, double size) {
-        gc.drawImage(KEY_IMAGE, x*size, y*size);
+        switch (this.keyColour) {
+            case RED -> gc.drawImage(KEY_IMAGE, x * size, y * size);
+            case GREEN -> gc.drawImage(KEY_IMAGE, x * size, y * size);
+            case BLUE -> gc.drawImage(KEY_IMAGE, x * size, y * size);
+            case YELLOW -> gc.drawImage(KEY_IMAGE, x * size, y * size);
+        }
+
     }
 }
