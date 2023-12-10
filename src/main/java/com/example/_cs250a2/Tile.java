be106f92311f05b4e14685ffd2d7e7d2eb0be998
jndmh <92041858+tinho-mp4@users.noreply.github.com>
2023-12-10 20:25:25 +0000

    checkstyle and javadoc Tile.java

--- src/main/java/com/example/_cs250a2/Tile.java
+++ src/main/java/com/example/_cs250a2/Tile.java
@@ -3,9 +3,8 @@ package com.example._cs250a2;
 import javafx.scene.canvas.GraphicsContext;
 
 /**
- * The {@code Tile} abstract class represents a framework for all items in the game
- * @author idk
- * @version 1.0
+ * An abstract base class for all tile-like elements in the game.
+ * This class provides a common framework for tile properties and behaviors.
  */
 public abstract class Tile {
     protected String name;
@@ -14,6 +13,14 @@ public abstract class Tile {
     protected boolean solid;
     protected boolean pushableBlock;
 
+    /**
+     * Constructs a Tile with a specified name, position, and solidity.
+     *
+     * @param name  Name of the tile.
+     * @param x     X-coordinate of the tile.
+     * @param y     Y-coordinate of the tile.
+     * @param solid Boolean indicating if the tile is solid (not passable).
+     */
     public Tile(String name, int x, int y, boolean solid) {
         this.name = name;
         this.x = x;
@@ -21,33 +28,77 @@ public abstract class Tile {
         this.solid = solid;
     }
 
+    /**
+     * Gets the X-coordinate of the tile.
+     *
+     * @return The X-coordinate.
+     */
     public int getX() {
         return x;
     }
 
+    /**
+     * Gets the Y-coordinate of the tile.
+     *
+     * @return The Y-coordinate.
+     */
     public int getY() {
         return y;
     }
 
+    /**
+     * Checks if the tile is solid (not passable).
+     *
+     * @return true if the tile is solid, false otherwise.
+     */
     public boolean isSolid() {
         return solid;
     }
 
+    /**
+     * Sets the solidity of the tile.
+     *
+     * @param solid Boolean indicating the new solid state.
+     */
     public void setSolid(boolean solid) {
         this.solid = solid;
     }
 
+    /**
+     * Checks if the tile is a pushable block.
+     *
+     * @return true if the tile is pushable, false otherwise.
+     */
     public boolean isPushableBlock() {
         return pushableBlock;
     }
 
+    /**
+     * Sets the pushable state of the tile.
+     *
+     * @param pushableBlock Boolean indicating the new pushable state.
+     */
     public void setPushableBlock(boolean pushableBlock) {
         this.pushableBlock = pushableBlock;
     }
 
+    /**
+     * Gets the name of the tile.
+     *
+     * @return The name of the tile.
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * Abstract method to draw the tile.
+     * This method should be implemented by subclasses to define how the tile is drawn.
+     *
+     * @param gc   GraphicsContext for drawing the tile.
+     * @param x    X-coordinate for drawing.
+     * @param y    Y-coordinate for drawing.
+     * @param size Size of the tile.
+     */
     public abstract void draw(GraphicsContext gc, double x, double y, double size);
 }
