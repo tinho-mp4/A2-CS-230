24a841ec2b6f02d23aec817a2201abc4f3d620d7
jndmh <92041858+tinho-mp4@users.noreply.github.com>
2023-12-10 23:35:13 +0000

    - HighScoreManager.java Checkstyle Javadoc Done

--- src/main/java/com/example/_cs250a2/HighScoreManager.java
+++ src/main/java/com/example/_cs250a2/HighScoreManager.java
@@ -5,46 +5,70 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Manages the high scores for different levels in the game.
+ * Stores and updates scores, and provides access to the top scores.
+ */
 public class HighScoreManager {
 
-    private final int MAX_SCORES = 10;
     private final Map<String, List<ScoreEntry>> highScores;
 
+    /**
+     * Constructs a new HighScoreManager.
+     */
     public HighScoreManager() {
         highScores = new HashMap<>();
     }
 
+    /**
+     * Adds a score entry for a specific level.
+     * If the score qualifies as a high score, it is added to the list for that level.
+     *
+     * @param levelName   The name of the level.
+     * @param profileName The name of the player's profile.
+     * @param score       The score to be added.
+     */
     public void addScore(final String levelName, final String profileName, final int score) {
-        List<ScoreEntry> scores = highScores.get(levelName);
+        List<ScoreEntry> scores = highScores.computeIfAbsent(levelName, k -> new ArrayList<>());
 
-        if (scores == null) {
-            scores = new ArrayList<>();
-            highScores.put(levelName, scores);
-        }
-
-        if (scores.size() < MAX_SCORES || score > getLowestScore(scores)) {
+        int maxScores = 10;
+        if (scores.size() < maxScores || score > getLowestScore(scores)) {
             scores.add(new ScoreEntry(profileName, score));
 
             scores.sort((s1, s2) -> Integer.compare(s2.getScore(), s1.getScore()));
 
-            if (scores.size() > MAX_SCORES) {
-                scores.remove(MAX_SCORES);
+            if (scores.size() > maxScores) {
+                scores.remove(maxScores);
             }
         }
     }
 
+    /**
+     * Retrieves the list of high scores for a given level.
+     *
+     * @param levelName The name of the level.
+     * @return A list of ScoreEntry objects representing the high scores for that level.
+     */
     public List<ScoreEntry> getHighScores(final String levelName) {
         return highScores.getOrDefault(levelName, new ArrayList<>());
     }
 
+    /**
+     * Saves the high scores for a specific level to a file.
+     *
+     * @param highScores The list of high scores to save.
+     * @param levelName  The name of the level.
+     */
     public void saveHighScores(List<ScoreEntry> highScores, String levelName) {
         HighScoreFileManager.saveHighScores(highScores, levelName);
     }
 
-    public List<ScoreEntry> loadHighScores(String levelName) {
-        return HighScoreFileManager.loadHighScores(levelName);
-    }
-
+    /**
+     * Gets the lowest score in a list of score entries.
+     *
+     * @param scores The list of ScoreEntry objects.
+     * @return The lowest score found in the list.
+     */
     private int getLowestScore(final List<ScoreEntry> scores) {
         if (scores.isEmpty()) {
             return 0;
