c8477d2b889f8de0567cb638b3b26e3e35d5f2ce
jndmh <92041858+tinho-mp4@users.noreply.github.com>
2023-12-10 21:26:46 +0000

    - Java doc and checkstyle LockedDoor.java Done - Deleted unused methods in Profile.java

--- src/main/java/com/example/_cs250a2/Profile.java
+++ src/main/java/com/example/_cs250a2/Profile.java
@@ -2,12 +2,7 @@ package com.example._cs250a2;
 
 import java.io.*;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.Comparator;
-import java.util.Collections;
-import java.util.ArrayList;
 
 /**
  * Handles player profiles in the game.
@@ -20,10 +15,8 @@ public class Profile implements Serializable {
 
     private final String name;
     private int levelReached;
-    private Map<String, Integer> levelScores;
-    private Map<String, PriorityQueue<ScoreEntry>> highScores;
+    private final Map<String, Integer> levelScores;
     public static final int MAX_LEVEL = 5;
-    public static final int LEVEL_HIGH_SCORE = 10;
 
     /**
      * Creates a new profile with the given name.
@@ -34,7 +27,6 @@ public class Profile implements Serializable {
         this.name = name;
         this.levelReached = MAX_LEVEL;
         this.levelScores = new HashMap<>();
-        this.highScores = new HashMap<>();
     }
 
     /**
@@ -57,14 +49,6 @@ public class Profile implements Serializable {
     }
 
     /**
-     * Set the highest level reached by the player.
-     * @param levelReached  The highest level reached by the player.
-     */
-    public void setLevelReached(int levelReached) {
-        this.levelReached = levelReached;
-    }
-
-    /**
      * Get the score for the given level.
      * @param level The level to get the score for.
      */
@@ -79,67 +63,6 @@ public class Profile implements Serializable {
      */
     public void setScoreForLevel(String level, int score) {
         levelScores.put(level, score);
-    }
-
-    /**
-     * Saves the profile to a file.
-     *
-     * @param filePath The path to the file to save the profile to.
-     */
-    public void saveToFile(String filePath) {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filePath))) {
-            oos.writeObject(this);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Load the profile from a file.
-     * @param filePath The path to the file to load the profile from.
-     */
-    private void loadFromFile(String filePath) {
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filePath))) {
-            Profile savedProfile = (Profile) ois.readObject();
-            this.levelReached = savedProfile.levelReached;
-            this.levelScores = savedProfile.levelScores;
-            this.highScores = savedProfile.highScores;
-        } catch (FileNotFoundException e) {
-            // Ignore if the file doesn't exist (first run)
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    /**
-     * Add a score to the high scores for the given level.
-     * @param level The level to add the score to.
-     * @param score The score to add.
-     */
-    public void addScoreToHighScores(String level, int score) {
-        highScores.putIfAbsent(level, new PriorityQueue<>(Comparator.comparingInt(ScoreEntry::getScore)));
-        PriorityQueue<ScoreEntry> levelHighScores = highScores.get(level);
-        if (levelHighScores.size() < LEVEL_HIGH_SCORE || score > levelHighScores.peek().getScore()) {
-            if (levelHighScores.size() == LEVEL_HIGH_SCORE) {
-                levelHighScores.poll();
-            }
-            levelHighScores.add(new ScoreEntry(name, score));
-        }
-    }
-
-    /**
-     * Get the high scores for the given level.
-     * @param level The level to get the high scores for.
-     * @return The high scores for the given level.
-     */
-    public List<ScoreEntry> getHighScores(String level) {
-        if (!highScores.containsKey(level)) {
-            return Collections.emptyList();
-        }
-        List<ScoreEntry> scores = new ArrayList<>(highScores.get(level));
-        scores.sort(Comparator.comparingInt(ScoreEntry::getScore).reversed());
-        return scores;
     }
 
     /**
